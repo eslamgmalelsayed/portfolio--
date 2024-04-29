b8dfd4e1fac1c04eb6804cb4543d65187551ddfe
islam <eslamgmal1@gmail.com>
2024-04-30 00:02:35 +0300

    Revert and fix "push app to main"
    This reverts commit 3dbf83902b78549544b0d3c43909f066d016dc99.

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from "./plugins/vuetify.js"
-import i18n from "./locales/initLocales.js"
-
-createApp(App).use(i18n).use(vuetify).mount('#app')
